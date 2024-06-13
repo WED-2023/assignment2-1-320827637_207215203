97d4d094dee3b6e713af571ecbbc563d1b2f555e
marikf98 <marikf98@gmail.com>
2024-06-13 18:09:23 +0300

    assdf

--- src/pages/RecipeViewPage.vue
+++ src/pages/RecipeViewPage.vue
@@ -1,55 +1,17 @@
 <template>
   <div class="container">
-    <div v-if="recipe">
-      <div class="recipe-header mt-3 mb-4">
-        <h1>{{ recipe.title }}</h1>
-        <img :src="recipe.image" class="center" />
-      </div>
-      <div class="recipe-body">
-        <div class="wrapper">
-          <div class="wrapped">
-            <div class="mb-3">
-              <div>Ready in {{ recipe.readyInMinutes }} minutes</div>
-              <div>Likes: {{ recipe.aggregateLikes }} likes</div>
-              <div>Servings: {{ recipe.servings }}</div>
-            </div>
-            <div class="mb-3">
-              <div>Vegetarian: {{ recipe.vegetarian ? "Yes" : "No" }}</div>
-              <div>Vegan: {{ recipe.vegan ? "Yes" : "No" }}</div>
-              <div>Gluten Free: {{ recipe.glutenFree ? "Yes" : "No" }}</div>
-            </div>
-            Ingredients:
-            <ul>
-              <li
-                v-for="(r, index) in recipe.extendedIngredients"
-                :key="index + '_' + r.id"
-              >
-                {{ r.original }}
-              </li>
-            </ul>
-          </div>
-          <div class="wrapped">
-            Instructions:
-            <ol>
-              <li v-for="s in recipe._instructions" :key="s.number">
-                {{ s.step }}
-              </li>
-            </ol>
-          </div>
-        </div>
-      </div>
-      <!-- <pre>
-      {{ $route.params }}
-      {{ recipe }}
-    </pre
-      > -->
-    </div>
+    <FullRecipe :recipe="recipe"/>
   </div>
 </template>
 
 <script>
-import { mockGetRecipeFullDetails } from "../services/recipes.js";
+import {mockGetRecipeFullDetails} from "../services/recipes.js";
+import FullRecipe from '../components/FullRecipe.vue';
+
 export default {
+  components: {
+    FullRecipe
+  },
   data() {
     return {
       recipe: null
@@ -58,34 +20,12 @@ export default {
   async created() {
     try {
       let response;
-      // response = this.$route.params.response;
+      response = mockGetRecipeFullDetails(this.$route.params.recipeId);
+      console.log('Response:', response);
 
-      try {
-        // response = await this.axios.get(
-        //   this.$root.store.server_domain + "/recipes/" + this.$route.params.recipeId,
-        //   {
-        //     withCredentials: true
-        //   }
-        // );
-
-        response = mockGetRecipeFullDetails(this.$route.params.recipeId);
-        console.log('Response:', response); // Add this line
-
-        // console.log("response.status", response.status);
-        // if (response.status !== 200) {
-        //   this.$router.replace("/NotFound");
-        // }
-        if (!response.data || !response.data.recipe) {
-          console.log('Response data or recipe is not defined'); // Add this line
-          this.$router.replace("/NotFound");
-        }
-      }
-      catch (error)
-      {
-        console.log('Error fetching recipe details:', error);
-        console.log("error.response.status", error.response.status);
+      if (!response.data || !response.data.recipe) {
+        console.log('Response data or recipe is not defined');
         this.$router.replace("/NotFound");
-        return;
       }
 
       let {
@@ -103,11 +43,11 @@ export default {
       } = response.data.recipe;
 
       let _instructions = analyzedInstructions
-        .map((fstep) => {
-          fstep.steps[0].step = fstep.name + fstep.steps[0].step;
-          return fstep.steps;
-        })
-        .reduce((a, b) => [...a, ...b], []);
+          .map((fstep) => {
+            fstep.steps[0].step = fstep.name + fstep.steps[0].step;
+            return fstep.steps;
+          })
+          .reduce((a, b) => [...a, ...b], []);
 
       let _recipe = {
         instructions,
@@ -125,10 +65,8 @@ export default {
       };
 
       this.recipe = _recipe;
-    }
-    catch (error)
-    {
-      console.log('Error in created() method:', error); // Add this line
+    } catch (error) {
+      console.log('Error in created() method:', error);
       console.log(error);
     }
   }
@@ -142,51 +80,5 @@ export default {
   align-items: center;
   margin: 0 auto;
   max-width: 800px;
-}
-
-.recipe-header {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-.recipe-header h1 {
-  font-size: 2em;
-  color: #333;
-}
-
-.recipe-header img {
-  width: 100%;
-  height: auto;
-  border-radius: 10px;
-}
-
-.recipe-body {
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-}
-
-.wrapper {
-  display: flex;
-  justify-content: space-between;
-  margin-top: 20px;
-}
-
-.wrapped {
-  width: 45%;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-}
-
-.wrapped ul, .wrapped ol {
-  padding-left: 20px;
-}
-
-.center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
 }
 </style>
