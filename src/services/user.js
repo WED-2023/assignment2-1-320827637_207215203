595bbdb23bf0336f28c907bdb1687a06029cbd65
marikf98 <marikf98@gmail.com>
2024-09-11 13:02:25 +0300

    log in and register front end

--- src/services/user.js
+++ src/services/user.js
@@ -3,13 +3,23 @@ import recipe_full_view from "../assets/mocks/recipe_full_view.json";
 import recipe_preview from "../assets/mocks/recipe_preview.json";
 
 
-  export function mockAddFavorite(recipeId) {
-    return { status: 200, response: { data: { message: "The Recipe successfully saved as favorite", success: true}} };
-  }
+  // export function mockAddFavorite(recipeId) {
+  //   return { status: 200, response: { data: { message: "The Recipe successfully saved as favorite", success: true}} };
+  // }
   
-  export function mockAddUserRecipe(recipeDetails) {
-    return { status: 200, response: { data: { message: "The Recipe successfully added to My Recipes", success: true}} };
+  // export function mockAddUserRecipe(recipeDetails) {
+  //   return { status: 200, response: { data: { message: "The Recipe successfully added to My Recipes", success: true}} };
 
+  // }
+  
+  import axios from 'axios';
+  const API_URL = 'http://localhost:3000'; // Adjust the URL as needed
+
+  export async function createPersonalRecipe(recipe) {
+    try {
+      const response = await axios.post(`${API_URL}/createPersonalRecipe`, recipe);
+      return response.data;
+    } catch (error) {
+      throw error.response.data;
+    }
   }
-  
-  
